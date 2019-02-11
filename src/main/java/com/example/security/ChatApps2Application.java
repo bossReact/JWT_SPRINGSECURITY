f28060e963c66f523d1fa1d1e171d762889082b9
Saurabh <java.saurabhbhurle@gmail.com>
2019-02-11 04:35:06 -0500

    initial commit

---
+++ src/main/java/com/example/security/ChatApps2Application.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class ChatApps2Application {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApps2Application.class, args);
+    }
+     
+}
